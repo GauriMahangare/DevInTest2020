cc60c2391d839ced00387873d3e97d90d58c73b7
Gauri <gauridhumal@gmail.com>
2020-03-29 17:58:52 +0100

    Version 2

---
+++ M1 - DevOps - JUnit/src/com/aes/devintest/jUnit/AppUtil.java
@@ -0,0 +1,16 @@
+package com.aes.devintest.jUnit;
+
+public class AppUtil {
+
+    public int add(int x, int y){
+        System.out.println("adding "+x +" and " +y);
+        return x+y;
+    }
+
+    public int multiply(int x, int y){
+        System.out.println("Multiplying "+x + " and " +y);
+        return x*y;
+    }
+
+
+}
